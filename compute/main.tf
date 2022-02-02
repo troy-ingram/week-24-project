766d61ab5fe9ae2b194cb1156f6387ef16b74efd
joeyrogo <67221823+joeyrogo@users.noreply.github.com>
2022-02-02 08:47:36 -0500

    Update main.tf

--- compute/main.tf
+++ compute/main.tf
@@ -31,9 +31,9 @@ resource "aws_launch_template" "web" {
 resource "aws_autoscaling_group" "web" {
   name                = "web"
   vpc_zone_identifier = tolist(var.public_subnet)
-  min_size            = 2
-  max_size            = 3
-  desired_capacity    = 2
+  min_size            = 25
+  max_size            = 100
+  desired_capacity    = 25
 
   launch_template {
     id      = aws_launch_template.web.id
